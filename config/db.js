583a6f3fdffed6c7c566fc80baf56e9f4dd10527
viniciustmota <motaviny140@gmail.com>
2025-03-29 12:06:15 -0300

    Tentando fazer o projeto subir na web!!!!

--- config/db.js
+++ config/db.js
@@ -1,6 +1,5 @@
 const mongoURI = process.env.NODE_ENV === 'production'
-  ? "mongodb+srv://motaviny140:1234@blogapp-prod.51yv1sl.mongodb.net/blogapp?retryWrites=true&w=majority"
-  : "mongodb://localhost/blogapp"
-
+    ? "mongodb+srv://motaviny140:1234@blogapp-prod.51yv1sl.mongodb.net/"
+    : "mongodb://localhost/blogapp"
 
 export default { mongoURI }
