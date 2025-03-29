65d391de58ecdf506782899c59f778ee0578d466
viniciustmota <motaviny140@gmail.com>
2025-03-29 11:55:13 -0300

    Arrumando o bancod de dados

--- config/db.js
+++ config/db.js
@@ -1,5 +1,6 @@
 const mongoURI = process.env.NODE_ENV === 'production'
-    ? "mongodb+srv://motaviny140:1234@blogapp-prod.51yv1sl.mongodb.net/"
-    : "mongodb://localhost/blogapp"
+  ? "mongodb+srv://motaviny140:1234@blogapp-prod.51yv1sl.mongodb.net/blogapp?retryWrites=true&w=majority"
+  : "mongodb://localhost/blogapp"
+
 
 export default { mongoURI }
